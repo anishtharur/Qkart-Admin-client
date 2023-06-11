e90d9038343d98797b90ab896c450b44b5c5f68b
Anish <anishtharur@gmail.com>
2023-06-11 21:25:56 +0530

    RTQ added

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -5,10 +5,13 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import NavBar from "components/NavBar";
 import SideBar from "components/SideBar";
+import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width : 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const userId = useSelector((state) => state.global.userId);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
