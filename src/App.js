b5c1529501ac922ec3a2a6c432cf597ae2d3ddc2
Anish <anishtharur@gmail.com>
2023-06-24 20:46:59 +0530

    charts added for monthly

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
-import Daily from "scenes/Daily";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -32,6 +33,7 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
